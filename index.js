e5a66df57359332d52ac2e87e8756124b077f883
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-16 09:35:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code here
+function  breakfast(meal, drink) {
+   this.meal=meal;
+   this.drink = drink;
+   this.sayHello = function() {
+     console.log(`${this.year},${this.drink}`);
+   };
+ }
+   let breakfast = new breakfast('eggs', 'juice')
+   breakfast.breakfast();
+
+   function lunch (salade,soup,drink) {
+     this.salade=salade;
+     this.soup=soup;
+     this.drink = drink;
+     this.sayHello = function() {
+       console.log(`${this.salade},${this.soup},${this.drink}`);
+     };
+   }
+   let newlunch = new lunch('side salad','broccoli cheddar soup','iced tea')
+     newlunch.lunch();
+
+  function dinner (salade,soup,starter,dessert) {
+         this.salade = salade;
+         this.soup = soup;
+         this.starter = starter;
+         this._dessert = dessert;
+         this.sayHello = function() {
+           console.log(`${this.soup},${this.starter},${this._dessert}`);
+         };
+       }
+     let newdinner = new dinner('balsamic salad', 'consomme', 'filet mignon', 'cheesecake')
+         newdinner.dinner();
